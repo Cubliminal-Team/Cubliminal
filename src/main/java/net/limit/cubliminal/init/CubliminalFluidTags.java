743c3a756a977a5dccde66f605f67c33b8a45c5f
jonathan-x01 <jonjam1208@gmail.com>
2025-07-25 18:21:52 -0400

    - Fixed fluid's fog

--- src/main/java/net/limit/cubliminal/init/CubliminalFluidTags.java
+++ src/main/java/net/limit/cubliminal/init/CubliminalFluidTags.java
@@ -1,19 +1,15 @@
 package net.limit.cubliminal.init;
 
 import net.limit.cubliminal.Cubliminal;
-import net.limit.cubliminal.util.DebugLogger;
+import net.limit.cubliminal.Initer;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 
-public class CubliminalFluidTags {
+public class CubliminalFluidTags implements Initer {
     public static final TagKey<Fluid> CUSTOM_FLUIDS = of("custom_fluids");
 
     private static TagKey<Fluid> of(String id) {
         return TagKey.of(RegistryKeys.FLUID, Cubliminal.id(id));
-    }
-
-    public static void init() {
-        DebugLogger.displayRegisteredSectors(CubliminalFluidTags.class);
     }
 }
