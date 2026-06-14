305658650112467b6b5086f51d9f1475fec87e3e
ImBanana <eladtairo@gmail.com>
2026-06-14 12:51:49 +0300

    Add the main ai for the skin stealer entity

--- src/main/java/net/limit/cubliminal/entity/hostile/SkinStealerEntity.java
+++ src/main/java/net/limit/cubliminal/entity/hostile/SkinStealerEntity.java
@@ -3,6 +3,8 @@ package net.limit.cubliminal.entity.hostile;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -20,6 +22,7 @@ import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.EnumSet;
 import java.util.UUID;
 
 public class SkinStealerEntity extends HostileEntity implements Angerable {
@@ -29,10 +32,16 @@ public class SkinStealerEntity extends HostileEntity implements Angerable {
             SkinStealerEntity.ATTACKING_SPEED_MODIFIER_ID, 0.15f, EntityAttributeModifier.Operation.ADD_VALUE
     );
     private static final TrackedData<Boolean> ANGRY = DataTracker.registerData(SkinStealerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
-    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(70, 80);
+    private static final TrackedData<Boolean> IN_DISGUISED = DataTracker.registerData(SkinStealerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final UniformIntProvider DISGUISED_TIME_RANGE = TimeHelper.betweenSeconds(40, 60);
+
+
     private int angerTime;
     @Nullable
     private UUID angryAt;
+
+    private int disguisedTime;
 
     public SkinStealerEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
@@ -51,11 +60,13 @@ public class SkinStealerEntity extends HostileEntity implements Angerable {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.0, false));
+        this.goalSelector.add(2, new SkinStealerEntity.AttackGoal(this, 1.0, false));
+        this.goalSelector.add(3, new SkinStealerEntity.FollowVictimGoal(this, 1.0, 1.0f));
+        this.goalSelector.add(4, new SkinStealerEntity.RevealSelfGoal(this, 28));
         this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0, 0.0f));
         this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0f));
         this.goalSelector.add(8, new LookAroundGoal(this));
-        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, false, false, this::shouldAngerAt));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, 0, false, false, null));
         this.targetSelector.add(2, new RevengeGoal(this));
         this.targetSelector.add(4, new UniversalAngerGoal<>(this, false));
     }
@@ -77,27 +88,53 @@ public class SkinStealerEntity extends HostileEntity implements Angerable {
     }
 
     @Override
+    public boolean onKilledOther(ServerWorld world, LivingEntity other) {
+        if (other instanceof PlayerEntity player) {
+            this.disguiseAs(player);
+        }
+        return super.onKilledOther(world, other);
+    }
+
+    public void disguiseAs(PlayerEntity player) {
+        this.setDisguisedTime(DISGUISED_TIME_RANGE.get(this.random));
+        this.setInDisguised(true);
+    }
+
+    public void revealSelf() {
+        this.setInDisguised(false);
+    }
+
+    @Override
     protected void initDataTracker(DataTracker.Builder builder) {
         super.initDataTracker(builder);
         builder.add(ANGRY, false);
+        builder.add(IN_DISGUISED, false);
     }
 
     @Override
     public void writeCustomDataToNbt(NbtCompound nbt) {
         super.writeCustomDataToNbt(nbt);
         this.writeAngerToNbt(nbt);
+        nbt.putBoolean("InDisguised", this.isInDisguised());
+        nbt.putInt("DisguisedTime", this.getDisguisedTime());
     }
 
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
         super.readCustomDataFromNbt(nbt);
         this.readAngerFromNbt(this.getWorld(), nbt);
+        this.setInDisguised(nbt.getBoolean("InDisguised"));
+        this.setDisguisedTime(nbt.getInt("DisguisedTime"));
     }
 
     @Override
     public void tickMovement() {
         if (!this.getWorld().isClient) {
             this.tickAngerLogic((ServerWorld)this.getWorld(), true);
+
+            if (this.disguisedTime > 0) {
+                this.disguisedTime -= 1;
+            }
         }
         super.tickMovement();
     }
@@ -130,5 +167,136 @@ public class SkinStealerEntity extends HostileEntity implements Angerable {
     @Override
     public void chooseRandomAngerTime() {
         this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+
+    public boolean isInDisguised() {
+        return this.dataTracker.get(IN_DISGUISED);
+    }
+
+    public void setInDisguised(boolean value) {
+        this.dataTracker.set(IN_DISGUISED, value);
+    }
+
+    public int getDisguisedTime() {
+        return disguisedTime;
+    }
+
+    public void setDisguisedTime(int disguisedTime) {
+        this.disguisedTime = disguisedTime;
+    }
+
+    private static class FollowVictimGoal extends Goal {
+        private final SkinStealerEntity skinStealer;
+        private final double speed;
+        private final EntityNavigation navigation;
+        private final float minDistance;
+
+        private PlayerEntity target;
+        private float oldWaterPathFindingPenalty;
+        private int updateCountdownTicks;
+
+        public FollowVictimGoal(SkinStealerEntity skinStealer, double speed, float minDistance) {
+            this.skinStealer = skinStealer;
+            this.speed = speed;
+            this.navigation = skinStealer.getNavigation();
+            this.minDistance = minDistance;
+            this.setControls(EnumSet.of(Control.MOVE, Control.LOOK));
+        }
+
+        @Override
+        public boolean canStart() {
+            UUID targetUUID = this.skinStealer.getAngryAt();
+            if (targetUUID == null) return false;
+            this.target = this.skinStealer.getWorld().getPlayerByUuid(targetUUID);
+            return this.skinStealer.isInDisguised() && this.skinStealer.isAngry() && this.target != null;
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            return this.target != null && this.skinStealer.isInDisguised() && !this.navigation.isIdle() && this.skinStealer.squaredDistanceTo(this.target) > this.minDistance * this.minDistance;
+        }
+
+        @Override
+        public void start() {
+            this.updateCountdownTicks = 0;
+            this.oldWaterPathFindingPenalty = this.skinStealer.getPathfindingPenalty(PathNodeType.WATER);
+            this.skinStealer.setPathfindingPenalty(PathNodeType.WATER, 0.0F);
+        }
+
+        @Override
+        public void stop() {
+            this.target = null;
+            this.navigation.stop();
+            this.skinStealer.setPathfindingPenalty(PathNodeType.WATER, this.oldWaterPathFindingPenalty);;
+        }
+
+        @Override
+        public void tick() {
+            if (this.target != null && !this.skinStealer.isLeashed()) {
+                this.skinStealer.getLookControl().lookAt(this.target, 10.0F, this.skinStealer.getMaxLookPitchChange());
+                if (--this.updateCountdownTicks <= 0) {
+                    this.updateCountdownTicks = this.getTickCount(10);
+                    double xDist = this.skinStealer.getX() - this.target.getX();
+                    double yDist = this.skinStealer.getY() - this.target.getY();
+                    double zDist = this.skinStealer.getZ() - this.target.getZ();
+                    double dist = xDist * xDist + yDist * yDist + zDist * zDist;
+                    if (dist > this.minDistance * this.minDistance) {
+                        this.navigation.startMovingTo(this.target, this.speed);
+                    } else {
+                        this.navigation.stop();
+                    }
+                }
+            }
+        }
+    }
+
+    private static class AttackGoal extends MeleeAttackGoal {
+        private final SkinStealerEntity skinStealer;
+
+        public AttackGoal(SkinStealerEntity mob, double speed, boolean pauseWhenMobIdle) {
+            super(mob, speed, pauseWhenMobIdle);
+            this.skinStealer = mob;
+        }
+
+        @Override
+        public boolean canStart() {
+            return super.canStart() && !this.skinStealer.isInDisguised();
+        }
+    }
+
+    private static class RevealSelfGoal extends Goal {
+        private final SkinStealerEntity skinStealer;
+        private final float maxDistance;
+        private PlayerEntity target;
+
+        private RevealSelfGoal(SkinStealerEntity skinStealer, float maxDistance) {
+            this.skinStealer = skinStealer;
+            this.maxDistance = maxDistance;
+        }
+
+        @Override
+        public boolean canStart() {
+            UUID targetUUID = this.skinStealer.getAngryAt();
+            if (targetUUID == null) return false;
+            this.target = this.skinStealer.getWorld().getPlayerByUuid(targetUUID);
+            return this.skinStealer.isInDisguised() && this.target != null;
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            return this.skinStealer.isInDisguised() && this.target != null;
+        }
+
+        @Override
+        public boolean shouldRunEveryTick() {
+            return true;
+        }
+
+        @Override
+        public void tick() {
+            if (this.skinStealer.distanceTo(this.target) >= this.maxDistance || this.skinStealer.getDisguisedTime() <= 0) {
+                this.skinStealer.revealSelf();
+            }
+        }
     }
 }
